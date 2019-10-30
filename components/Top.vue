1b7e8c8f59fe7f46f018e16ad35b2e72da58d9ed
daisuke <dsaito85a@gmail.com>
2019-10-31 07:06:18 +0900

    NEWSの仮作成

--- components/Top.vue
+++ components/Top.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- TODO: 画面いっぱいに広がるように拡張したい -->
-    <v-parallax dark src="/images/bg.jpg" height="1000">
+    <v-parallax dark src="/images/bg.jpg" height="600">
       <v-row align="center" justify="center">
         <v-col class="text-center" cols="12">
           <h1 class="display-4 font-weight-black mb-4">SOUL SPICE</h1>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <top />
+    <news />
   </div>
 </template>
 
 <script>
 import Top from '~/components/Top.vue'
+import News from '~/components/News.vue'
 export default {
   components: {
-    Top
+    Top,
+    News
   }
 }
 </script>
