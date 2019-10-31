dd8209c015c66bb4b666f962a84478cb0228e6bf
daisuke <dsaito85a@gmail.com>
2019-10-31 11:53:52 +0900

    Aboutページの作成

---
+++ components/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- TODO: 画面いっぱいに広がるように拡張したい -->
+    <!-- レスポンシブレイアウトに応じてheightを調節したい -->
+    <v-parallax dark src="/images/bg.jpg" height="400">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h2 class="display-2 font-weight-black">[ ABOUT ]</h2>
+        </v-col>
+        <v-col class="text-left" cols="12">
+          <h4 class="title font-weight-black">
+            小平を中心に活動しているキッズダンスサークルです
+          </h4>
+          <p>
+            活動をはじめてから１０年が経ちました！<br />
+            小学校１年生から中学校３年生までのキッズ達が、 楽しく活動
+            しています。 ほとんどの子供たちが初心者からダンスを始めました。<br />
+            ヒップホップを中心に、ロック、ハウスとなかなかキッズダンスでは習えないダンスに挑戦しています。<br />
+            ３～４ヶ月に１回はイベントに参加し、みんなの練習の成果を発表する場を設けていますので、子ども達のモチベーションもＵＰ♪<br />
+            子ども達はいつも次のイベントを楽しみにレッスンを受けています★<br />
+          </p>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div>
     <top />
     <news />
+    <about />
   </div>
 </template>
 
 <script>
 import Top from '~/components/Top.vue'
 import News from '~/components/News.vue'
+import About from '~/components/About.vue'
 export default {
   components: {
     Top,
-    News
+    News,
+    About
   }
 }
 </script>
