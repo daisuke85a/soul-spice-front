792fd8b9da8a728b6c14a4afcede17cf6f0cfcaa
daisuke <dsaito85a@gmail.com>
2019-11-01 22:42:16 +0900

    動画の追加

---
+++ components/Movie.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <fade-in-up-in-screen>
+          <h2 class="display-2 font-weight-black">[ MOVIE ]</h2>
+        </fade-in-up-in-screen>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col class="embed-responsive embed-responsive-16by9" cols="12">
+        <iframe
+          class="embed-responsive-item"
+          src="https://www.youtube.com/embed/videoseries?list=PLJgPtg4l-yVZ8FlF_mMu439KD6322ytoj&amp;ecver=1"
+          frameborder="0"
+          allow="autoplay; "
+          allowfullscreen
+        ></iframe>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<style scoped>
+.embed-responsive-16by9 {
+  padding-bottom: 56.25%;
+}
+.embed-responsive {
+  position: relative;
+  display: block;
+  height: 0;
+  overflow: hidden;
+}
+.embed-responsive-item {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     <about />
     <instructor />
     <schedule />
+    <movie />
   </div>
 </template>
 
@@ -14,13 +15,15 @@ import News from '~/components/News.vue'
 import About from '~/components/About.vue'
 import Instructor from '~/components/Instructor.vue'
 import Schedule from '~/components/Schedule.vue'
+import Movie from '~/components/Movie.vue'
 export default {
   components: {
     Top,
     News,
     About,
     Instructor,
-    Schedule
+    Schedule,
+    Movie
   }
 }
 </script>
