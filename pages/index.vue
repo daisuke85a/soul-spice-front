1a5462dd47be1cbe45ba37c20cb7264fd1474b8c
daisuke <dsaito85a@gmail.com>
2020-01-14 19:43:36 +0900

    feat: ナビゲーションメニューでスクロールするよう修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
-    <top />
-    <news />
-    <about />
-    <instructor />
-    <schedule />
-    <movie />
-    <contact />
+    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          router
+          exact
+          @click="$vuetify.goTo(item.to)"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-btn
+      v-show="$vuetify.breakpoint.mdAndDown"
+      fab
+      color="light-blue"
+      top
+      left
+      fixed
+      @click.stop="drawer = !drawer"
+    >
+      <v-icon color="white">mdi-menu</v-icon>
+    </v-btn>
+    <top id="home" />
+    <news id="news" />
+    <about id="about" />
+    <instructor id="instructor" />
+    <schedule id="schedule" />
+    <movie id="movie" />
+    <contact id="contact" />
   </div>
 </template>
 
@@ -27,6 +56,51 @@ export default {
     Schedule,
     Movie,
     Contact
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: null,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'ホーム',
+          to: '#home' // TODO:スクロールに修正する
+        },
+        {
+          icon: 'mdi-information-outline',
+          title: '最新情報',
+          to: '#news'
+        },
+        {
+          icon: 'mdi-circle-outline',
+          title: 'サークル紹介',
+          to: '#about'
+        },
+        {
+          icon: 'mdi-account-supervisor-circle',
+          title: 'コーチ紹介',
+          to: '#instructor'
+        },
+        {
+          icon: 'mdi-calendar',
+          title: 'スケジュール',
+          to: '#schedule'
+        },
+        {
+          icon: 'mdi-movie-open-outline',
+          title: 'メンバー動画',
+          to: '#movie'
+        },
+        {
+          icon: 'mdi-contact-mail-outline',
+          title: 'お問い合わせ',
+          to: '#contact'
+        }
+      ],
+      title: 'SoulSpice'
+    }
   }
 }
 </script>
