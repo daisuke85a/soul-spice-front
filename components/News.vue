7eb56701df4dab0a19fe89c4480fc8039a084ce7
daisuke <dsaito85a@gmail.com>
2020-01-15 13:04:41 +0900

    feat: 複数コンテンツの取得に対応

--- components/News.vue
+++ components/News.vue
@@ -2,31 +2,31 @@
   <div>
     <v-row align="center" justify="center">
       <v-col class="text-center" cols="12">
-        <fade-in-up-in-screen>
-          <h2 class="display-2 font-weight-black pa-5">[ NEWS ]</h2>
-        </fade-in-up-in-screen>
+        <h2 class="display-2 font-weight-black pa-5">[ NEWS ]</h2>
       </v-col>
     </v-row>
     <!-- TODO: ３行以上の場合はアコーディオンにしたい -->
     <v-row v-for="n in 1" :key="n" class="mb-1" no-gutters>
-      <v-col v-for="k in 3" :key="k" class="col-12 col-sm-4">
-        <fade-in-up-in-screen>
-          <v-card class="pa-2 ma-2 mx-auto" outlined tile>
-            <v-img class="white--text align-end" :src="image">
-              <!-- TODO: テキストの背景をグレーの透過色にしておしゃれにしたい -->
-              <v-card-title class="font-weight-bold py-0">
-                {{ title }}
-              </v-card-title>
-            </v-img>
-            <v-card-subtitle class="pb-0">{{ date }}</v-card-subtitle>
-            <v-card-text class="text--primary">
-              <!-- TODO: テキストが一定以上の場合はアコーディオンにしたい -->
-              <div>
-                {{ paragraph }}
-              </div>
-            </v-card-text>
-          </v-card>
-        </fade-in-up-in-screen>
+      <v-col
+        v-for="content in contents"
+        :key="content.id"
+        class="col-12 col-sm-4"
+      >
+        <v-card class="pa-2 ma-2 mx-auto" outlined tile>
+          <v-img class="white--text align-end" :src="content.image">
+            <!-- TODO: テキストの背景をグレーの透過色にしておしゃれにしたい -->
+            <v-card-title class="font-weight-bold py-0">
+              {{ content.title }}
+            </v-card-title>
+          </v-img>
+          <v-card-subtitle class="pb-0">{{ content.date }}</v-card-subtitle>
+          <v-card-text class="text--primary">
+            <!-- TODO: テキストが一定以上の場合はアコーディオンにしたい -->
+            <div>
+              {{ content.paragraph }}
+            </div>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </div>
@@ -36,10 +36,7 @@ const axios = require('axios')
 export default {
   data() {
     return {
-      title: null,
-      date: null,
-      image: 'init', // nullだとimageの読み込みに失敗してエラーになるから初期化
-      paragraph: null
+      contents: []
     }
   },
   async mounted() {
@@ -49,15 +46,24 @@ export default {
       }
     })
     console.log(news)
-    this.title = news.data.contents[0].title
 
-    const date = new Date(news.data.contents[0].date)
-    const formatted = `${date.getFullYear()}-${date.getMonth() +
-      1}-${date.getDate()}`.replace(/\n|\r/g, '')
+    for (let i = 0, len = news.data.contents.length; i < len; i++) {
+      const image = news.data.contents[i].image.url
+      const date = new Date(news.data.contents[i].date)
+      const formatted = `${date.getFullYear()}-${date.getMonth() +
+        1}-${date.getDate()}`.replace(/\n|\r/g, '')
 
-    this.date = formatted
-    this.image = news.data.contents[0].image.url
-    this.paragraph = news.data.contents[0].paragraph
+      const contents = {
+        id: news.data.contents[i].id,
+        title: news.data.contents[i].title,
+        date: formatted,
+        image,
+        paragraph: news.data.contents[i].paragraph
+      }
+
+      this.contents.push(contents)
+    }
+    console.log(this.contents)
   }
 }
 </script>
